b36af13a02ec1c19cc1b77d2ae644db9d9257683
Mayan <vivekmishra261105@gmail.com>
2026-01-09 17:50:55 +0530

    Fix api folder structure for Vercel

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
+
+const routes = require("../routes");
+app.use("/", routes);
+
+module.exports = app;
