47c0dd9b0485c848b34b1955b2a56447ded8022b
Rudra-clrscr <rpsbareilly06@gmail.com>
2026-01-19 23:13:45 +0530

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const Order = require('../models/order');
 
 // Admin login page
